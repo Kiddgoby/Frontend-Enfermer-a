1e069e9abd987d60b87ad2faeb3beb9dfcb1900a
Arnau Gausachs <arnau.gausachs.gancedo@gmail.com>
2025-11-28 15:55:11 +0100

    conflictos solucionados

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,16 +1,12 @@
 import { Routes } from '@angular/router';
 import { NurseSearch } from './nurse-search/nurse-search';
-export const routes: Routes = [
-    {path: 'nurse-search', component: NurseSearch   }
-
 import { Home } from './home/home';
-
-export const routes: Routes = [
-  {path: '', component: Home },
 import { NurseList } from './nurse-list/nurse-list';
 import { App } from './app';
 
-export const routes: Routes = [    
+export const routes: Routes = [
+    {path: '', component: Home },
+    {path: 'nurse-search', component: NurseSearch},
     {
         path: ' ',
         component: App ,
