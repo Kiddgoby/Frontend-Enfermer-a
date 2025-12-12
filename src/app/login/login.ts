8d548d1316936cc3bb723b633cd8ebe807fa1979
Olalla <olallasanchezbernad08@gmail.com>
2025-12-12 16:23:48 +0100

    validacion con ngModel

--- src/app/login/login.ts
+++ src/app/login/login.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms'; 
-import { CommonModule } from '@angular/common'; 
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
@@ -9,17 +9,32 @@ import { CommonModule } from '@angular/common';
   styleUrl: './login.css',
 })
 export class Login {
+
   username: string = '';
   password: string = '';
-  loginStatus: string = ''; 
+  loginStatus: string = '';
+
+  // Datos simulados del enfermero
+  private nurseUser = [
+    {username: 'olalla', password: 'olalla'},
+    {username: 'arnau', password: 'arnau'},
+    {username: 'javier', password: 'javier'},
+    {username: 'david', password: 'david'}
+  ];
 
   login() {
+    // Validar campos vacíos
     if (!this.username || !this.password) {
       this.loginStatus = 'empty';
       return;
     }
 
-    if (this.username === 'enfermero' && this.password === '1234') {
+    // Validar con los datos locales simulados
+    const foundUser = this.nurseUser.find(
+      user => user.username === this.username && user.password === this.password
+    );
+
+    if (foundUser) {
       this.loginStatus = 'success';
     } else {
       this.loginStatus = 'error';
