605093a63a33f7c35931fa5f65588650d42317ca
Olalla <olallasanchezbernad08@gmail.com>
2025-12-13 23:26:20 +0100

    busqueda con users arreglados

---
+++ src/app/login-service/login-service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginService {
+
+  private loggedUser: string | null = null;
+
+  constructor() {}
+
+  setUser(username: string): void {
+    this.loggedUser = username;
+  }
+
+  logout(): void {
+    this.loggedUser = null;
+  }
+
+  isLoggedIn(): boolean {
+    return this.loggedUser !== null;
+  }
+
+  getUser(): string | null {
+    return this.loggedUser;
+  } 
+}
--- src/app/login/login.ts
+++ src/app/login/login.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router'; 
-import { LoginService } from '../login-service/login-service';  
+import { LoginService } from '../login-service/login-service';
+
 
 @Component({
   selector: 'app-login',
@@ -17,7 +18,6 @@ export class Login {
   password: string = '';
   loginStatus: string = '';
 
-  // Datos simulados del enfermero
   private nurseUser = [
     { name: "Javier", last_name: "Givica", user: "JaGiFe", password: "1234" },
     { name: "Arnau", last_name: "Gaushachs", user: "ArGaGa", password: "abcd" },
@@ -30,7 +30,7 @@ export class Login {
     { name: "Biel", last_name: "Turias", user: "Byel", password: "4321" }
   ];
 
-  constructor(private loginService: LoginService, private router: Router) {}
+  constructor(private router: Router, private loginService: LoginService) {}
   login() {
     if (!this.username || !this.password) {
       this.loginStatus = 'empty';
@@ -38,15 +38,17 @@ export class Login {
     }
 
     const foundUser = this.nurseUser.find(
-      user => user.user.toLowerCase() === this.username.toLowerCase() && user.password === this.password
+      user => user.user === this.username && user.password === this.password
     );
 
-    if (foundUser) {
+  if (foundUser) {
     this.loginStatus = 'success';
+
     this.loginService.setUser(foundUser.user);
-    this.router.navigate(['/nurse-search']); 
-    } else {
-      this.loginStatus = 'error';
-    }
+
+    this.router.navigate(['./nurse-search']);
+  } else {
+    this.loginStatus = 'error';
+  }
   }
 }
