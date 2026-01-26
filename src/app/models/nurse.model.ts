9cecfee277c9a596d99223d29519cd3b8739d378
Arnau Gausachs <arnau.gausachs.gancedo@gmail.com>
2026-01-26 15:20:26 +0100

    comit - todo funciona de nurselist

--- src/app/models/nurse.model.ts
+++ src/app/models/nurse.model.ts
@@ -1,15 +1,13 @@
 export class Nurse {
     id?: number;
     name: string;
-    surname: string;
     username: string;
     password: string;
     passwordConfirm: string;
     img: string;
 
-    constructor(name: string, surname: string, username: string, password: string, passwordConfirm: string, img: string) {
+    constructor(name: string, username: string, password: string, passwordConfirm: string, img: string) {
         this.name = name;
-        this.surname = surname;
         this.username = username;
         this.password = password;
         this.passwordConfirm = passwordConfirm;
